c8b2868b9b8be3dca7da1ce2447fd3200fca9e76
Clickady73917 <111101343+Clickady73917@users.noreply.github.com>
2022-10-08 10:53:26 -0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BLACKGAMER1221/Pet-Simulator-X/main/BK%20Pet.lua"))()
+wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZaRdoOx/Ultra-Hub/main/Main"))()
+wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/31"))()
+print("Thanks for using this script made by Clickady!")
+--Some credits to other people to for the gui's, i made the loader.--
